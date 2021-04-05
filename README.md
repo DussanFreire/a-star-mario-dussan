8c7741c5cd77c98a6818cdb8904da96c49c39c66
Dussan Freire <73646620+DussanFreire@users.noreply.github.com>
2021-04-04 23:47:24 -0400

    readme

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ This web application has two basic interfaces:
 * After several experiments, we got the following conclusions:
   * A*, using Radar heuristic, is the one that opens fewer states. However, the A* function is also the one that take more time to find the solution. This heuristic has a much better performance when there is not obstacles 
   * A*, using Rect line and Near borders Heuristic, have almost the same results in all the cases.
-  * BFS, takes more time and also more states in almost all the cases
+  * BFS, takes more time and also more states in almost all the cases (more states are expanded and more time is taken )
   * The difference between A* and BFS are less, when the map has mane obstacles 
     
         
--- mario_map/mario_board/heuristic_factory.py
+++ mario_map/mario_board/heuristic_factory.py
@@ -17,24 +17,24 @@ class HeuristicFactory:
         if state.father is not None:
             if (state.father.position.col == state.position.col == successor.position.col) or (
                     state.father.position.row == state.position.row == successor.position.row):
-                return 1
-        return 3
+                return (board.dimensions.num_cols * board.dimensions.num_rows) + 1
+        return (board.dimensions.num_cols * board.dimensions.num_rows) + 3
 
     @staticmethod
     def near_borders_h(successor, state, board):
         if successor.position.col == 0 or successor.position.col == board.dimensions.num_cols - 2:
-            return 1
+            return (board.dimensions.num_cols * board.dimensions.num_rows) + 1
         if successor.position.row == 0 or successor.position.row == board.dimensions.num_rows - 2:
-            return 1
+            return  (board.dimensions.num_cols * board.dimensions.num_rows) +1
         else:
-            return 2
+            return (board.dimensions.num_cols * board.dimensions.num_rows) + 2
 
     @staticmethod
     def radar_h(successor, state, board):
         if HeuristicFactory.pipe_position is None or state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
             HeuristicFactory.pipe_position = HeuristicFactory._get_positions_of_closest_pipe(state.position, board)
             if HeuristicFactory.pipe_position is None:
-                return 1
+                return (board.dimensions.num_cols * board.dimensions.num_rows) + 1
         pipe_quadrant = HeuristicFactory._classified_pipe(HeuristicFactory.pipe_position, state.position)
         if pipe_quadrant == "down_right":
             if successor.position.col == state.position.col + 1 or successor.position.row == state.position.row + 1:
@@ -44,7 +44,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 2 + state.f
-                return 2
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +2
         if pipe_quadrant == "upper_left":
             if successor.position.col == state.position.col - 1 or successor.position.row == state.position.row - 1:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -53,7 +53,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 2 + state.f
-                return 2
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +2
         if pipe_quadrant == "upper_right":
             if successor.position.col == state.position.col + 1 or successor.position.row == state.position.row - 1:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -62,7 +62,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 2 + state.f
-                return 2
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +2
         if pipe_quadrant == "down_left":
             if successor.position.col == state.position.col - 1 or successor.position.row == state.position.row + 1:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -71,7 +71,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 2 + state.f
-                return 2
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +2
         if pipe_quadrant == "down":
             if successor.position.col == state.position.col and successor.position.row == state.position.row + 1:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -80,7 +80,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 1 + state.f
-                return 1
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +1
         if pipe_quadrant == "up":
             if successor.position.col == state.position.col and successor.position.row == state.position.row - 1:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -89,7 +89,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 1 + state.f
-                return 1
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +1
         if pipe_quadrant == "left":
             if successor.position.col == state.position.col - 1 and successor.position.row == state.position.row:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -98,7 +98,7 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 1 + state.f
-                return 1
+                return (board.dimensions.num_cols * board.dimensions.num_rows) +1
         if pipe_quadrant == "right":
             if successor.position.col == state.position.col + 1 and successor.position.row == state.position.row:
                 if state.f == 0 or state.f == HeuristicFactory.pipe_searching_interval:
@@ -107,8 +107,8 @@ class HeuristicFactory:
                         state.position.col - HeuristicFactory.pipe_position.col) else abs(
                         state.position.col - HeuristicFactory.pipe_position.col)
                     HeuristicFactory.pipe_searching_interval = perception_scope + 1 + state.f
-                return 1
-        return 8
+                return (board.dimensions.num_cols * board.dimensions.num_rows) + 1
+        return (board.dimensions.num_cols * board.dimensions.num_rows) + 8
 
     @staticmethod
     def _classified_pipe(pipe_position, state_position):
